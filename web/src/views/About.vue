94a2195b211aef85b4c1f86430534be5ba1b6f51
geekwhocodes <ganeshraskar@outlook.com>
2020-12-05 12:29:03 +0530

    add about view and vuex store

---
+++ web/src/views/About.vue
@@ -0,0 +1,91 @@
+<template>
+  <section class="dashboard content">
+    <div class="container" style="position: relative">
+      <b-loading v-if="isLoadingUserList" active :is-full-page="false" />
+        <b-tabs>
+            <b-tab-item label="Table">
+                <b-table
+                    :data="users"
+                    :columns="columns"
+                    :checked-rows.sync="checkedRows"
+                    checkable
+                    :checkbox-position="checkboxPosition">
+
+                    <template slot="bottom-left">
+                        <b>Total checked</b>: {{ checkedRows.length }}
+                    </template>
+                </b-table>
+            </b-tab-item>
+
+            <b-tab-item label="Checked rows">
+                <pre>{{ checkedRows }}</pre>
+            </b-tab-item>
+        </b-tabs>
+    </div>
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios';
+
+export default {
+  name: 'Settings',
+  components: {
+  },
+
+  data() {
+    return {
+      users: [],
+      errors: [],
+      isLoadingUserList: true,
+      checkboxPosition: 'left',
+      checkedRows: [],
+      columns: [
+        {
+          field: 'id',
+          label: 'ID',
+          numeric: true,
+        },
+        {
+          field: 'firstname',
+          label: 'First Name',
+        },
+        {
+          field: 'lastname',
+          label: 'Last Name',
+        },
+        {
+          field: 'email',
+          label: 'Email',
+          centered: false,
+        },
+        {
+          field: 'website',
+          label: 'Webiste',
+          centered: false,
+        },
+      ],
+    };
+  },
+  async mounted() {
+    try {
+      // const headers = {
+      //   'Content-Type': 'application/json',
+      //   'Access-Control-Allow-Origin': '*',
+      // };
+      axios.defaults.baseURL = 'http://localhost:8081';
+      const response = await axios.get('/api/users');
+      this.users = response.data;
+      console.log(response.data);
+      this.isLoadingUserList = false;
+    } catch (e) {
+      this.errors.push(e);
+      this.isLoadingUserList = false;
+    }
+    // this.isLoadingUserList = false;
+  },
+  computed: {
+  },
+};
+</script>
